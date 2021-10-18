e8a5758f74a3f4c33772298011187ba497eae259
alibayram <alibayram88@gmail.com>
2021-10-19 00:02:15 +0300

    18 Ekim ders sonu

---
+++ src/app.js
@@ -0,0 +1,24 @@
+console.log("Merhaba Kodlama.io")
+
+let dolarDun = 9.20
+let dolarBugun = 9.50
+{
+        let dolarDun=9.10
+}
+console.log(dolarDun)
+console.log(dolarBugun)
+
+const euroDun = 11.2
+//const ile sabit tanımlanır. 
+// camelCasing
+//PascalCasing
+console.log(euroDun)
+let konutKredileri =["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi"]
+console.log("<ul>")
+for(let i= 0; i<konutKredileri.length; i++)
+{
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
+console.log("</ul>")
+
+console.log(konutKredileri)
